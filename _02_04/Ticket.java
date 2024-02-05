6bff311321bda5451a314467d3015ecabe2b9437
Brennan Harvey <brenharvey@aol.com>
2024-02-05 19:10:57 +0000

    Classes and Objects

--- _02_04/Ticket.java
+++ _02_04/Ticket.java
@@ -12,8 +12,30 @@ public class Ticket {
 
   // Add three methods to set the value of each field, called setDestination,
   // setPrice and setIsReturn.
+  public void SetDestiation(String destination) {
+    this.destination = destination;
+  }
+
+  public void SetPrice(double price) {
+    this.price = price;
+  }
+
+  public void SetIsReturn(boolean isReturn) {
+    this.isReturn = isReturn;
+  }
 
   // Add a separate method to get the value of each field, called getDestination,
   // getPrice and getIsReturn.
+  public String getDestiation() {
+    return this.destination;
+  }
+
+  public double getPrice() {
+    return this.price;
+  }
+
+  public boolean getIsReturn() {
+    return this.isReturn;
+  }
 
 }
